41011f3676858315c1e899272506e20e7e401d2b
Nick Duckwiler <nduckwiler@gmail.com>
2022-07-16 21:09:50 -0400

    Initialize Tone.Transport

--- public/javascripts/Drumroll.js
+++ public/javascripts/Drumroll.js
@@ -40,6 +40,11 @@ let drumrollSketch = function (p5) {
         p5.centerCanvas();
     };
 
+    p5.mouseClicked = async function() {
+      await Tone.start()
+      gameManager.transport.start()
+    }
+
     //render
 
 
@@ -52,5 +57,38 @@ gameManager.backgroundColor = '#222222';
 gameManager.transport = Tone.Transport;
 console.log(gameManager.transport);
 
-
 //TODO: Model initialization
+gameManager.transport.loop = true;
+gameManager.transport.loopStart = "0:0:0";
+gameManager.transport.loopEnd = "16:0:0";
+gameManager.transport.schedule((time) => {
+  console.log(`${time}: starting song`)
+}, "0:0:0");
+
+// Define sampler
+const sampler = new Tone.Sampler({
+  urls: {
+    "C4": "C4.mp3",
+    "D#4": "Ds4.mp3",
+    "F#4": "Fs4.mp3",
+    "A4": "A4.mp3",
+  },
+  release: 1,
+  baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
+
+// Wait for samples to load...
+Tone.loaded().then(() => {
+  
+  const emptySequenceArray = [].fill(null, 64);
+  let someSequenceArray = emptySequenceArray;
+  someSequenceArray[0] = "Eb4";
+  someSequenceArray[15] = "Eb4";
+  someSequenceArray[31] = "Eb4";
+  someSequenceArray[47] = "Eb4";
+  const sampleSeq = new Tone.Sequence((time, note) => {
+    sampler.triggerAttackRelease(note, 0.1, time);
+    // sequences are defined as 64 beat arrays
+  }, someSequenceArray).start("0:0:0")
+
+});
